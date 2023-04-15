71b4586bb863ec9c56f43be982616632ef288502
Matt Arau <matt.arau@timecount.com>
2023-04-15 16:41:37 +0200

    chore(install): add bare repo setup script

---
+++ .install/README.md
@@ -0,0 +1,7 @@
+# dotfiles
+
+## Setup
+
+```bash
+curl -L https://raw.githubusercontent.com/mattarau/dotfiles/main/.install/setup.sh | /bin/bash
+```
---
+++ .install/setup.sh
@@ -0,0 +1,20 @@
+# Based on the Atlassian Bare Repo tutorial
+# https://www.atlassian.com/git/tutorials/dotfiles
+
+# Clone the dotfiles bare repo
+git clone --bare https://github.com/mattarau/dotfiles.git $HOME/.cfg
+
+# Define the config alias as a function for this script's shell scope
+function config() {
+	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME "$@"
+}
+
+# Backup any conflicting files
+mkdir -p .config-backup
+config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+
+# Copy the config files
+config checkout
+
+# Set the config repo to not show untracked files
+config config --local status.showUntrackedFiles no
