eef78c900590f1beb87f8c513d34c069a25d052c
Matt Arau <matt@arau.pro>
2023-12-15 01:34:16 +0100

    fix(install): check if dotfiles bare repo dir already exists
    Also asks to remove it and backups up the conflicting files

--- .install/setup.sh
+++ .install/setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ $((git --version) 2>&1) == *"requesting install"* ]]; then
-  echo
+if [[ $( (git --version) 2>&1) == *"requesting install"* ]]; then
+	echo
 	echo "Git is not installed. Requesting install."
 	echo "Run this script again after it's done."
-  echo
+	echo
 
 	exit
 fi
@@ -12,18 +12,56 @@ fi
 # Based on the Atlassian Bare Repo tutorial
 # https://www.atlassian.com/git/tutorials/dotfiles
 
+REPO_PATH=$HOME/.cfg
+
+if [ -d "$REPO_PATH" ]; then
+	echo
+	echo "The dotfiles bare repo directory already exists."
+	echo
+	echo "Overwrite the content in the \`$REPO_PATH\` directory? [y/N]: "
+
+	read -r REPLY </dev/tty
+
+	case "$REPLY" in
+	y | Y)
+		rm -rf "$REPO_PATH"
+
+		;;
+	*)
+		echo "Nothing has been changed."
+
+		exit
+		;;
+	esac
+fi
+
 # Clone the dotfiles bare repo
-git clone --bare https://github.com/mattarau/dotfiles.git $HOME/.cfg
+git clone --bare https://github.com/mattarau/dotfiles.git "$REPO_PATH"
 
 # Define the config alias as a function for this script's shell scope
 # For global scope, an alias is defined in .config/zsh/aliases/dotfiles
 function dotfiles() {
-	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME "$@"
+	/usr/bin/git --git-dir="$REPO_PATH" --work-tree="$HOME" "$@"
 }
 
 # Backup any conflicting files
-mkdir -p .config-backup
-dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+BACKUP_PATH=$HOME/.config-backup
+mkdir -p "$BACKUP_PATH"
+dotfiles checkout 2>&1 | grep -E "\s+\.|README.md" | awk \{'print $1'\} | tr '\n' '\0' | xargs -0 sh -c '
+echo 
+for file_path; do
+  GREEN="\033[0;32m"
+  ORANGE="\033[0;33m"
+  NC="\033[0m" # No Color
+
+  BACKUP_PATH="~/.config-backup/$file_path"
+  
+  echo "moving ${ORANGE}~/$file_path${NC} to ${GREEN}$BACKUP_PATH${NC}"
+  
+  mkdir -p "$(dirname $BACKUP_PATH)"
+  mv "$file_path" "$BACKUP_PATH"
+done
+' _ # Multi-command solution from: https://stackoverflow.com/a/6958957
 
 # Copy the config files
 dotfiles checkout
@@ -35,6 +73,6 @@ dotfiles config --local remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-source $SCRIPT_DIR/homebrew.sh
+source "$SCRIPT_DIR"/homebrew.sh
 
-source $SCRIPT_DIR/git.sh
+source "$SCRIPT_DIR"/git.sh
