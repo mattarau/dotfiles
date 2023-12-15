57b544aa94012adbd83bd924a6ac139f647d3a9e
Matt Arau <matt@arau.pro>
2023-12-16 00:38:29 +0100

    feat(editor): add initial custom vim options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,44 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local options = {
+  backup = false, -- creates a backup file
+  colorcolumn = "80", -- Line lenght marker at 80 columns
+  cmdheight = 2, -- more space in the neovim command line for displaying messages
+  fileencoding = "utf-8", -- the encoding written to a file
+  hlsearch = true, -- highlight all matches on previous search pattern
+  numberwidth = 2, -- set number column width to 2 {default 4}
+  showtabline = 2, -- always show tabs
+  swapfile = false, -- creates a swapfile
+  whichwrap = "bs<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line
+  writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn = "yes"
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- vim.opt.shortmess = "ilmnrx"                        -- flags to shorten vim messages, see :help 'shortmess'
+vim.opt.shortmess:append("c") -- don't give |ins-completion-menu| messages
+vim.opt.iskeyword:append("-") -- hyphenated words recognized by searches
+vim.opt.formatoptions:remove({ "c", "r", "o" }) -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
+vim.opt.runtimepath:remove("/usr/share/vim/vimfiles") -- separate vim plugins from neovim in case vim still in use
+
